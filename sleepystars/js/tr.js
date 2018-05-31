dd935ee988c883e775e8125e0c44c0d3e8e08fd7
tr ringstaff <trringstaff@iComputer.local>
2018-05-31 11:23:43 -0400

    __add code to hide sleepy header if coming from app

--- sleepystars/js/tr.js
+++ sleepystars/js/tr.js
@@ -26,6 +26,26 @@ $(document).ready(function(){
   //   }, 500);
   // };
 
+
+  function getQueryVariable(variable)
+  {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+           var pair = vars[i].split("=");
+           if(pair[0] == variable){return pair[1];}
+    }
+   return(false);
+  }
+
+  var isApp = getQueryVariable("app_ref");
+
+  if(isApp === "true") {
+    $('.bg-img').hide();
+    $('.header-tohide').hide();
+
+  }
+
   $(function() {
     $('a.page-scroll').bind('click', function(event) {
       var $anchor = $(this);
