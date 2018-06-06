09b0511ed6699f9ad28d233868d08d4fb979a246
Travis Ringstaff <ringstaff@traviss-mbp.lan>
2018-06-05 21:11:48 -0400

    __i think i'm done except for play store link! glory hallelujah

--- js/sleepy.js
+++ js/sleepy.js
@@ -40,6 +40,23 @@ $(document).ready(function(){
 
   var isApp = getQueryVariable("app_ref");
 
+  function slickIt() {
+    $('.fader').slick({
+      dots: false,
+      infinite: true,
+      speed: 800,
+      fade: true,
+      cssEase: 'linear',
+      arrows: false,
+      autoplay: true,
+      zIndex: 9,
+      autoplaySpeed: 200,
+    });
+  }
+
+  function unslickIt() {
+    $('.fader').slick('unslick');
+  }
   if(isApp === "true") {
     $('.bg-img').hide();
     $('.header-tohide').hide();
@@ -55,29 +72,19 @@ $(document).ready(function(){
 
     $('.navbar-toggler .fa-times').css('display', 'none');
 
+    slickIt();
 
-    $('.fader').slick({
-      dots: false,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear',
-      arrows: false,
-      autoplay: true,
-      zIndex: 9,
-      autoplaySpeed: 200,
-    });
   }
-
-  $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-          scrollTop: $($anchor.attr('href')).offset().top
-      }, 1500, 'easeInOutExpo');
-      event.preventDefault();
-    });
-  });
+  //
+  // $(function() {
+  //   $('a.page-scroll').bind('click', function(event) {
+  //     var $anchor = $(this);
+  //     $('html, body').animate({
+  //         scrollTop: $($anchor.attr('href')).offset().top
+  //     }, 1500, 'easeInOutExpo');
+  //     event.preventDefault();
+  //   });
+  // });
 
 
   var nav = $("#myNav");
@@ -111,11 +118,7 @@ $(document).ready(function(){
 
   loadHome();
 
-
-
-
-
-  // BOOTSTRAP 3.0 - Open YouTube Video Dynamicaly in Modal Window
+  // BOOTSTRAP 4.0 - Open YouTube Video Dynamicaly in Modal Window
   // Modal Window for dynamically opening videos
   $('a[href^="https://www.youtube.com"]').on('click', function(e){
     // Store the query string variables and values
@@ -161,6 +164,49 @@ $(document).ready(function(){
   	$('#mediaModal .modal-body').html('');
   });
 
+  // Select all links with hashes
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+  $('#mediaModal').on('show.bs.modal', function (event) {
+    unslickIt();
+  }).on('hide.bs.modal', function (event) {
+    slickIt();
+  })
 });
 
 // REQUIRED: "jQuery Query Parser" plugin.
